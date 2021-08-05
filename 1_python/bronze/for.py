7d9301f26cb7bb54a4849a829ed0e86ba65079a4
csmoong <csmoong@naver.com>
2021-08-05 23:45:24 +0900

    0805

---
+++ 1_python/bronze/for.py
@@ -0,0 +1,13 @@
+# # 10950
+# T = int(input())
+
+# for test in range(T):
+#     a, b = map(int, input().split())
+#     print(a + b)
+
+# 8393
+N = int(input())
+total = 0
+for n in range(N):
+    total += n+1
+print(total)
